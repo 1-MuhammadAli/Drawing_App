17da5d1b6ac8dd1f090fbcc0a57f52d71f310672
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-10 16:07:17 +0500

    Drawing View : Added -> the lines persist on the view

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -17,8 +17,9 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private  var mDrawPaint: Paint? = null
     private  var mCanvasPaint: Paint? = null
     private  var mBrushSize: Float = 0.toFloat()
-    private var color = Color.BLACK
-    private var canvas: Canvas? = null
+    private  var color = Color.BLACK
+    private  var canvas: Canvas? = null
+    private  val mPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
@@ -45,6 +46,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path,mDrawPaint!!)
+        }
         if(!mDrawPath!!.isEmpty){
             mCanvasPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -78,6 +85,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 }
             }
             MotionEvent.ACTION_UP ->{
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color,mBrushSize)
             }
             else -> return  false
